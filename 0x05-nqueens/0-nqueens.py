55049d83ea5a1f64f6b544220233e6d9f0f49d10
Nedudigital <segunchinedu2000@gmail.com>
2023-02-06 00:10:38 +0100

    new additions

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,107 +1,52 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
-"""
+"""Solving the N Queens problem"""
 import sys
 
 
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-pos = None
-"""The list of possible positions on the chessboard.
-"""
+args = sys.argv
+
+if len(args) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if args[1].isdigit() is False:
+    print("N must be a number")
+    exit(1)
+
+layout = int(args[1])
+if layout < 4:
+    print("N must be at least 4")
+    exit(1)
 
 
-def get_input():
-    """Retrieves and validates this program's argument.
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
-    n = 0
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
+def check_here(nlist, index, curr_num):
+    for item in nlist:
+        if index == item[1] or\
+                index == item[1] - (curr_num - item[0]) or\
+                index == item[1] + (curr_num - item[0]):
+            return 1
+    return list([curr_num, index])
 
 
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
-    Args:
-        pos0 (list or tuple): The first queen's position.
-        pos1 (list or tuple): The second queen's position.
-    Returns:
-        bool: True if the queens are in an attacking position else False.
-    """
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+def loop_n_find(nlist, curr_num):
+    if (curr_num > layout):
+        return
+
+    if len(nlist) == layout:
+        print(nlist)
+        return
+
+    j = 0
+    while j < layout:
+        val = check_here(nlist, j, curr_num)
+        if type(val) is list:
+            new_nlist = nlist.copy()
+            new_nlist.append(val)
+            loop_n_find(new_nlist, curr_num + 1)
+        j += 1
+    return
 
 
-def group_exists(group):
-    """Checks if a group exists in the list of solutions.
-    Args:
-        group (list of integers): A group of possible positions.
-    Returns:
-        bool: True if it exists, otherwise False.
-    """
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem.
-    Args:
-        row (int): The current row in the chessboard.
-        group (list of lists of integers): The group of valid positions.
-    """
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """Gets the solutions for the given chessboard size.
-    """
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+for i in range(layout):
+    arr = [[0, i]]
+    loop_n_find(arr, 1)
